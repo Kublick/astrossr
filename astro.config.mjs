71e610cfc75ad5c276c540471e4635d58192afe0
Max Blancarte <blancarte@gmail.com>
2025-02-23 13:03:46 -0800

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
-import node from "@astrojs/node";
+import awsAmplify from "astro-aws-amplify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
-
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: awsAmplify(),
+  output: "server",
 });
